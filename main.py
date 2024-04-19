4e0bbd5e8a1bceb24868395fda071906841291a4
Makcim897 <152578968+Makcim897@users.noreply.github.com>
2024-04-19 10:04:24 +0700

    I'm trying to make a transpose method
    I'm trying to make a transpose method

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "matrix-calculator"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    a11 = ft.TextField(value="0")
-    a12 = ft.TextField(value="0")
-    a13 = ft.TextField(value="0")
-    a21 = ft.TextField(value="0")
-    a22 = ft.TextField(value="0")
-    a23 = ft.TextField(value="0")
-    a31 = ft.TextField(value="0")
-    a32 = ft.TextField(value="0")
-    a33 = ft.TextField(value="0")
-    multiply_num = ft.TextField(value="0")
+    a11 = ft.TextField(value="", width=100)
+    a12 = ft.TextField(value="", width=100)
+    a13 = ft.TextField(value="", width=100)
+    a21 = ft.TextField(value="", width=100)
+    a22 = ft.TextField(value="", width=100)
+    a23 = ft.TextField(value="", width=100)
+    a31 = ft.TextField(value="", width=100)
+    a32 = ft.TextField(value="", width=100)
+    a33 = ft.TextField(value="", width=100)
+    multiply_num = ft.TextField(value="", width=105)
+    page.update()
 
-    def multiply():
+    def multiply(e):
         a11.value = str(int(a11.value) * multiply_num.value)
         a12.value = str(int(a12.value) * multiply_num.value)
         a13.value = str(int(a13.value) * multiply_num.value)
@@ -26,14 +27,21 @@ def main(page: ft.Page):
         a33.value = str(int(a33.value) * multiply_num.value)
         page.update()
 
+    def transpose(e):
+        a12.value = a21.value
+        a13.value = a31.value
+        a21.value = a12.value
+        a23.value = a32.value
+        a31.value = a13.value
+        a32.value = a23.value
 
-    page.add(ft.Row(controls=[a11, a12, a13]))
-    page.add(ft.Row(controls=[a21, a22, a23]))
-    page.add(ft.Row(controls=[a31, a32, a33]))
-    page.add(ft.Row(controls=[multiply_num,ft.TextButton(text="multiply", on_click=multiply())]))
+
+    page.add(ft.Row(controls=[a11, a12, a13], alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Row(controls=[a21, a22, a23], alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Row(controls=[a31, a32, a33], alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Row(controls=[multiply_num, ft.TextButton(text="multiply", on_click=multiply), ft.TextButton(text="transpose", on_click=transpose)],
+                    alignment=ft.MainAxisAlignment.CENTER))
 
     page.update()
-
-
 
 ft.app(target = main)
