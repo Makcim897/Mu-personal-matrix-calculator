333cb6470f48abd4e4b5c5bd65e22cb4d083a541
Makcim897 <152578968+Makcim897@users.noreply.github.com>
2024-05-02 21:43:28 +0700

    EARNED!!!
    Everything worked (as I wanted), and I added a function for calculating the matrix delta!

--- main.py
+++ main.py
@@ -13,35 +13,52 @@ def main(page: ft.Page):
     a32 = ft.TextField(value="", width=100)
     a33 = ft.TextField(value="", width=100)
     multiply_num = ft.TextField(value="", width=105)
+    vdelta = ft.TextField(value="", width=100)
     page.update()
 
     def multiply(e):
-        a11.value = str(int(a11.value) * multiply_num.value)
-        a12.value = str(int(a12.value) * multiply_num.value)
-        a13.value = str(int(a13.value) * multiply_num.value)
-        a21.value = str(int(a21.value) * multiply_num.value)
-        a22.value = str(int(a22.value) * multiply_num.value)
-        a23.value = str(int(a23.value) * multiply_num.value)
-        a31.value = str(int(a31.value) * multiply_num.value)
-        a32.value = str(int(a32.value) * multiply_num.value)
-        a33.value = str(int(a33.value) * multiply_num.value)
+        a11.value = int(a11.value) * int(multiply_num.value)
+        a12.value = int(a12.value) * int(multiply_num.value)
+        a13.value = int(a13.value) * int(multiply_num.value)
+        a21.value = int(a21.value) * int(multiply_num.value)
+        a22.value = int(a22.value) * int(multiply_num.value)
+        a23.value = int(a23.value) * int(multiply_num.value)
+        a32.value = int(a32.value) * int(multiply_num.value)
+        a33.value = int(a33.value) * int(multiply_num.value)
         page.update()
 
     def transpose(e):
-        a12.value = a21.value
-        a13.value = a31.value
-        a21.value = a12.value
-        a23.value = a32.value
-        a31.value = a13.value
-        a32.value = a23.value
+        copa21 = int(a21.value)
+        a21.value = int(a12.value)
+        a12.value = int(copa21)
+
+        copa13 = int(a13.value)
+        a13.value = int(a31.value)
+        a31.value = int(copa13)
+
+        copa23 = int(a23.value)
+        a23.value = int(a32.value)
+        a32.value = int(copa23)
+        page.update()
+
+
+    def delta(e):
+        vdelta.value =  ((int(a11.value)*int(a22.value)*int(a33.value))+
+                        (int(a21.value)*int(a32.value)*int(a13.value))+
+                        (int(a12.value)*int(a23.value)*int(a31.value))-
+                        (int(a13.value)*int(a22.value)*int(a31.value))-
+                        (int(a11.value)*int(a32.value)*int(a23.value))-
+                        (int(a12.value)*int(a21.value)*int(a33.value)))
+        page.update()
+
+
 
 
     page.add(ft.Row(controls=[a11, a12, a13], alignment=ft.MainAxisAlignment.CENTER))
     page.add(ft.Row(controls=[a21, a22, a23], alignment=ft.MainAxisAlignment.CENTER))
     page.add(ft.Row(controls=[a31, a32, a33], alignment=ft.MainAxisAlignment.CENTER))
-    page.add(ft.Row(controls=[multiply_num, ft.TextButton(text="multiply", on_click=multiply), ft.TextButton(text="transpose", on_click=transpose)],
-                    alignment=ft.MainAxisAlignment.CENTER))
-
+    page.add(ft.Row(controls=[multiply_num, ft.TextButton(text="multiply", on_click=multiply), ft.TextButton(text="transpose", on_click=transpose)],alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Row(controls=[vdelta, ft.TextButton(text="delta", on_click=delta)], alignment=ft.MainAxisAlignment.CENTER))
     page.update()
 
 ft.app(target = main)
